9b5473b09f279d2543e67939041e3ab62e99baf7
Alexander Vaisman <wisealex@tut.by>
2024-10-15 10:14:07 +0300

    case 5 - exposing internal class

--- include/api_updates/api.hpp
+++ include/api_updates/api.hpp
@@ -5,6 +5,10 @@
 
 namespace a{
 
+// case 5 - use forward declaration + shared_ptr
+class internal_class;
+using internal_class_sptr = std::shared_ptr<internal_class>;
+
 // case 2: update version namespace
 inline namespace v_1 {
     struct params {
@@ -26,6 +30,11 @@ inline namespace v_0 {
     };
     // non-inline function that uses some_class_interface that exposes f1 and f2 only
     void use_some_class(some_class_interface & arg);
+
+    // case 5 - provide a function that instantiates an object of internal_class
+    internal_class_sptr create_internal_class_instance(int value);
+    // case 5 - provide functions that redirect calls to internal_class methods
+    int get_value(internal_class_sptr const & class_ptr);
 }
 
 // inline part inside its own inline namespace
@@ -44,6 +53,16 @@ inline namespace inline_v_1 {
     private:
         int _m1;
         int _m2;
+    };
+
+    // case 5 - If you want to expose the functionality as a class
+    // provide a class that redirects calls to free functions
+    class exposed_internal_class{
+    public:
+        exposed_internal_class(int value) : _impl(a::create_internal_class_instance(value)){}
+        int get_value() { return a::get_value(_impl); } // redirect call to a free function
+    private:
+        internal_class_sptr  _impl;
     };
 }
 
